d6a598168548fd1d128fe3ff3721d2384bf7b812
Yoan Vandevelde <yvandevelde@antidots-group.com>
2023-10-15 21:55:24 +0200

    blog + rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://roaring-babka-c1c5db.netlify.app/',
+})
